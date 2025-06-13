4d79eee31fc85b2ec3d655759501382745056943
R3DChiLLi <112655230+R3DChiLLi@users.noreply.github.com>
2025-06-13 20:19:57 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,11 +3,15 @@
 REPO=$REPO
 ACCESS_TOKEN=$TOKEN
 
+# Generate a unique label for the runner based on the hostname or container ID
+LABEL="custom-$(hostname)"
+
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPO}/actions/runners/registration-token | jq .token --raw-output)
 
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --disableupdate --unattended --replace
+# Add the --labels option to assign the custom label
+./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --disableupdate --unattended --replace --labels ${LABEL}
 
 cleanup() {
     echo "Removing runner..."
